cef056393ae9037f61da941ac2c74615903f0fa7
Christopher Dorrell <dorrellkc@gmail.com>
2025-12-21 09:17:01 -0600

    Release v1.0.4

--- tux/apps/tux_tunes/__init__.py
+++ tux/apps/tux_tunes/__init__.py
@@ -7,6 +7,6 @@ that captures complete songs without cut-offs.
 Copyright (c) 2025 Christopher Dorrell. Licensed under GPL-3.0.
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __app_name__ = "Tux Tunes"
 __app_id__ = "com.tuxassistant.tuxtunes"
